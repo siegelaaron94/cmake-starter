4b5647a3ca7959050bbf5b4f65e02448cbf0ba35
Aaron Siegel <siegelaaron94@gmail.com>
2019-01-05 17:31:45 -0800

    Add basic Qt mainwindow.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# cmake-starter
-[![Build Status](https://travis-ci.org/siegelaaron94/cmake-starter.svg?branch=master)](https://travis-ci.org/siegelaaron94/cmake-starter)
+# cmake-starter (qt)
+[![Build Status](https://travis-ci.org/siegelaaron94/cmake-starter.svg?branch=qt)](https://travis-ci.org/siegelaaron94/cmake-starter)
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
-int main(int argc, char const *argv[])
+#include "MainWindow.h"
+
+#include <QApplication>
+
+const char *kApplicationName = "cmake-starter";
+const char *kOrganizationName = "siegelaaron94";
+
+int main(int argc, char *argv[])
 {
-	if constexpr(true)
-	{
-		return 0;
-	}
-	else
-	{
-		return 1;
-	}
+	QApplication app(argc, argv);
+	app.setApplicationName(kApplicationName);
+	app.setOrganizationName(kOrganizationName);
+
+	MainWindow window;
+	window.show();
+	
+	return app.exec();
 }
